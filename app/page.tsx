a7e45fd336383694132f3a66752ac207eabc4ed7
Alex Kyrylov <alex.kyrylov@dotcode.pro>
2025-12-29 21:24:58 +0200

    added test log

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,9 @@ export default function Home() {
   const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
+    console.log('test');
     loadFiles();
+
   }, []);
 
   useEffect(() => {
